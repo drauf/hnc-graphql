82e4c0f72dbda5b9d9dec7eeb068abc2a8f2d4f7
Daniel Rauf <danielr@kainos.com>
2017-12-16 15:08:40 +0100

    Add enters to make formatting consistent

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react'
 import LinkList from './LinkList'
 
 class App extends Component {
+
   render() {
     return (
       <LinkList />
     )
   }
+
 }
 
 export default App
