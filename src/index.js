c298d0d2b2d8f9ef810f9fb3120ff3d34bedd51f
Daniel Rauf <danielr@kainos.com>
2017-12-16 19:14:01 +0100

    Attach authorization header to Apollo requests

--- src/index.js
+++ src/index.js
@@ -5,16 +5,30 @@ import './styles/index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 
-import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
+import { ApolloLink } from 'apollo-client-preset'
+import { ApolloProvider } from 'react-apollo'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
-import { HTTP_URI } from './constants'
+import { GC_AUTH_TOKEN, HTTP_URI } from './constants'
 
 const httpLink = new HttpLink({ uri: HTTP_URI })
 
+const middlewareAuthLink = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem(GC_AUTH_TOKEN)
+  const authorizationHeader = token ? `Bearer ${token}` : null
+  operation.setContext({
+    headers: {
+      authorization: authorizationHeader
+    }
+  })
+  return forward(operation)
+})
+
+const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink)
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: httpLinkWithAuthToken,
   cache: new InMemoryCache()
 })
 
