efcc35efce1de78b65b8b8ee542e73a7a22a7b60
Daniel Rauf <danielr@kainos.com>
2017-12-16 14:53:08 +0100

    Remove semicolons

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
-const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cjb98nfp41sbs0174i9iky810' });
+const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cjb98nfp41sbs0174i9iky810' })
 
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache()
-});
+})
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>
-  , document.getElementById('root'));
-registerServiceWorker();
+  , document.getElementById('root'))
+registerServiceWorker()
